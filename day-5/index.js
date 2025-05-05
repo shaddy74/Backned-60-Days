795b41c97ebddd0e55bc4baaa68096842e698281
shaddy74 <sadab20012005@gmail.com>
2025-05-05 22:35:49 +0530

    NodeJS yaha tak complete hota hai or next ham ExpressJS Pe Focus krege

--- day-5/index.js
+++ day-5/index.js
@@ -1,6 +1,21 @@
-const crypto =require ('crypto')
+const crypto = require('crypto')
 
 // 1. random
-    const randomValues = crypto.randomBytes(1)
-    console.log(randomValues)
-// 2. crateHash
+const randomValues = crypto.randomBytes(16)
+console.log(randomValues.toString("hex"))
+
+
+// 2. createHash
+const hashValue = crypto.createHash("sha256").update("shaddy").digest("hex")
+
+const inputValue = "shaddy"
+const matchValue = crypto.createHash("sha256").update(inputValue).digest("hex")
+
+if (hashValue === matchValue) {
+    console.log("You can login")
+}
+else {
+    console.log("you can't login")
+}
+
+// learn about encryption & decryption
